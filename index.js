e5b64f6117c70b5f9bd92a6b9f8fa854c8481148
MuhammadNaufalGhulamF20230140073 <naufalfakhrudddin@gmail.com>
2025-10-20 09:55:10 +0700

    tambahkan koneksi MySQL dan jalankan server Express

--- index.js
+++ index.js
@@ -8,3 +8,15 @@ app.use(express.urlencoded({ extended: true }));
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+const db = myspl.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'Keluarga123',
+    database: 'mahasiswa',
+    port: 3309
+})
